6fa1aa3d20687e3299b58aa3bc90b31314b84103
Shodgearo <Shodkvest@gmail.com>
2018-12-02 13:11:54 +0600

    Override method for output background

--- src/Main.java
+++ src/Main.java
@@ -8,23 +8,36 @@ public class Main extends JFrame {
         new Main();
     }
 
-    final int WIDTH = 800;
-    final int HEIGHT = 600;
+    private final int WIDTH = 800;
+    private final int HEIGHT = 600;
+    private JPanel panel;
 
     public Main() {
         initWindow();
         initMainPanel();
+        addElements();
+    }
+
+    private void addElements() {
+        add(panel);
     }
 
     private void initMainPanel() {
+        panel = new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponents(g);
 
+                g.drawImage(new ImageIcon("Resource\\bg1.png").getImage(), 0, 0, this);
+            }
+        };
     }
 
     private void initWindow() {
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Arcanoid");
-        setResizable(false);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setResizable(false);
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
